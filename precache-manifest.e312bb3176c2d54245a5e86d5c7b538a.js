8b38730bd26ebd979857226df12a600ab9fd4909
qvnguyen <nguyenquocviet170898@gmail.com>
2019-02-24 12:19:51 -0500

    Updates

---
+++ precache-manifest.e312bb3176c2d54245a5e86d5c7b538a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64427c485b72af5154ef",
+    "url": "/random_quote_machine/static/js/runtime~main.64427c48.js"
+  },
+  {
+    "revision": "e5f72b54959b168ca51b",
+    "url": "/random_quote_machine/static/js/main.e5f72b54.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/random_quote_machine/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "e5f72b54959b168ca51b",
+    "url": "/random_quote_machine/static/css/main.1fad2189.chunk.css"
+  },
+  {
+    "revision": "8777a5cadc0b04723e561efb4f66c205",
+    "url": "/random_quote_machine/index.html"
+  }
+];
